2a01b1cfb5faca6d0bf135e5fbbe1f557ca3ddf6
conormcmenamin <conorm941@gmail.com>
2021-11-30 22:00:33 +0000

    Using typescript to communicate with API. TODO: Fix webpack file redirection

--- background.js
+++
@@ -1,8 +0,0 @@
-let sampleVAr = 'sample text';  //sets block scoped variable
-
-chrome.runtime.onInstalled.addListener(() => {   //chrome.runtime - API to retrieve the background page, return details about the manifest, and listen for and respond to events in the app or extension lifecycle.
-                                                        //Chrome event listener (onInstalled) runs when the extension is first installed, Chrome or Extension updated to new version
-    chrome.storage.sync.set({ sampleVar });                 //chrome.storage - API to store, retrieve, and track changes to user data.
-                                                        //By setting value with storage API it will allow multiple extension components to access that value and update it
-    console.log('sample log text');                //Writes to console
-});
---
+++ src/lib/app.ts
@@ -0,0 +1,41 @@
+import { getAccessToken, Token, songTracker} from "./spotify-interface";
+
+type divType= 'please-login-box'|'spotify-player-box';
+export type RepeatMode = 'track' | 'context' | 'off';
+
+export class App{
+    private track: songTracker;
+    private token: Token;
+    
+    constructor(){
+        this.token=null;
+        this.track=null;
+    }
+
+    public async render(){
+        this.token=await getAccessToken();
+        
+        if (!this.isLoggedIn()){
+            showUI('please-login-box');
+        }
+    }
+    private isLoggedIn(){
+        return this.token.isAnonymous;
+    }
+}
+
+function showUI(appMode: divType){
+    const loginNotification = document.getElementById('spotify-login-notification');
+    const player = document.getElementById('spotify-player');
+
+    switch(appMode){
+        case 'spotify-player-box':
+            player.style.display='flex';
+            loginNotification.style.display='none';
+            break;
+        case 'please-login-box':
+            player.style.display='none';
+            loginNotification.style.display='flex';
+            break;
+    }
+}
